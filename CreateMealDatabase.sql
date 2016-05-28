b4d7fa50448296372b382e7271c1484109caa292
mcmillinj <jamesmcmillin@Jamess-MacBook-Pro-2.local>
2016-05-28 17:43:31 +0100

    Database SQL Scripts Added
    Added script to set up the meals database from scratch, as well as a
    crib sheet of basic SQL commands.

---
+++ CreateMealDatabase.sql
@@ -0,0 +1,53 @@
+CREATE TABLE meal (
+	 MEAL_KEY    INT
+	,MEAL_NAME   VARCHAR(255)
+	,FREQUENCY   INT
+	,HEALTH      INT
+	,PORTIONS    INT
+);
+
+CREATE TABLE ingredient (
+	 INGREDIENT_KEY   INT
+	,INGREDIENT_NAME  VARCHAR(255)
+	,PRICE            INT
+	,CALORIES         INT
+);
+
+CREATE TABLE meal_ingredient_bridge (
+	 MEAL_KEY         INT
+	,INGREDIENT_KEY   INT
+);
+
+INSERT INTO meal VALUES (1, 'Chilli Con Carne', 5, 3, 4);
+INSERT INTO meal VALUES (2, 'Spaghetti Bolognese', 3, 3, 4);
+
+INSERT INTO ingredient VALUES (1, 'Minced Beef 500g', 400, 620);
+INSERT INTO ingredient VALUES (2, 'Onion', 20, 60);
+INSERT INTO ingredient VALUES (3, 'Garlic', 30, 6);
+INSERT INTO ingredient VALUES (4, 'Chopped Tomatoes 400g', 50, 100);
+INSERT INTO ingredient VALUES (5, 'Beef Stock (Knorr)', 35, 15);
+INSERT INTO ingredient VALUES (6, 'Jalapenos 100g', 65, 16);
+INSERT INTO ingredient VALUES (7, 'Kidney Beans 400g', 50, 280);
+INSERT INTO ingredient VALUES (8, 'Carrots x2', 10, 20);
+INSERT INTO ingredient VALUES (9, 'Parmesan 100g', 135, 400);
+INSERT INTO ingredient VALUES (10, 'Red Wine (glass)', 100, 120);
+INSERT INTO ingredient VALUES (11, 'Spaghetti 500g', 20, 1750);
+
+INSERT INTO meal_ingredient_bridge VALUES (1, 1);
+INSERT INTO meal_ingredient_bridge VALUES (1, 2);
+INSERT INTO meal_ingredient_bridge VALUES (1, 3);
+INSERT INTO meal_ingredient_bridge VALUES (1, 4);
+INSERT INTO meal_ingredient_bridge VALUES (1, 5);
+INSERT INTO meal_ingredient_bridge VALUES (1, 6);
+INSERT INTO meal_ingredient_bridge VALUES (1, 7);
+
+COMMIT;
+
+	
+SELECT SUM(i.price)
+FROM   meal m 
+JOIN   meal_ingredient_bridge mib 
+ON     m.meal_key = mib.meal_key 
+JOIN   ingredient i 
+ON     mib.ingredient_key = i.ingredient_key
+WHERE  m.meal_name = 'Chilli Con Carne';
---
+++ SqlCribSheet.sql
@@ -0,0 +1,55 @@
+--SQL Crib Sheet
+
+--How to connect
+
+/Applications/MAMP/Library/bin/mysql --host=localhost -uroot -proot
+
+--How to view databases
+
+SHOW DATABASES;
+
+--How to use a database
+
+USE DATABASE_NAME;
+
+--How to create a table
+
+CREATE TABLE test_table (column_1 INT, column_2 VARCHAR(255), column_3 INT);
+
+--How to add a row to a table
+
+INSERT INTO test_table VALUES (value_1, value_2, value_3);
+
+--How to select data from a table
+
+SELECT column_1, column_2 FROM test_table WHERE column_3 = 5;
+
+--How to select all columns from a table
+
+SELECT * FROM test_table;
+
+--How to describe the structure of a table
+
+DESC test_table;
+
+--How to change values in a table - very important to use a WHERE here!
+
+UPDATE test_table SET column_1 = 10 WHERE column_1 = 0;
+
+--How to add a column to a table
+
+ALTER TABLE test_table ADD column_4 INT;
+
+--How to remove a column from a table
+
+ALTER TABLE test_table DROP COLUMN column_4;
+
+--How to delete a table
+
+DROP TABLE test_table;
+
+--How to delete rows from a table - very important to use a WHERE here!
+
+DELETE FROM test_table WHERE column_1 = 1;
+
+
