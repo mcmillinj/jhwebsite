48a990df8486c306474d2edadcac74011be623ab
mcmillinj <jamesmcmillin@Jamess-MBP-2.lan>
2016-05-29 12:35:07 +0100

    Pasta Verde
    Added it to the meal database

--- CreateMealDatabase.sql
+++ CreateMealDatabase.sql
@@ -18,7 +18,6 @@ CREATE TABLE ingredient (
 	 INGREDIENT_KEY   INT
 	,INGREDIENT_NAME  VARCHAR(255)
 	,PRICE            INT
-	,CALORIES         INT
 );
 
 CREATE TABLE meal_ingredient_bridge (
@@ -28,18 +27,24 @@ CREATE TABLE meal_ingredient_bridge (
 
 INSERT INTO meal VALUES (1, 'Chilli Con Carne', 5, 3, 4);
 INSERT INTO meal VALUES (2, 'Spaghetti Bolognese', 3, 3, 4);
+INSERT INTO meal VALUES (3, 'Pasta Verde', 4, 3, 4);
 
-INSERT INTO ingredient VALUES (1, 'Minced Beef 500g', 400, 620);
-INSERT INTO ingredient VALUES (2, 'Onion', 20, 60);
-INSERT INTO ingredient VALUES (3, 'Garlic', 30, 6);
-INSERT INTO ingredient VALUES (4, 'Chopped Tomatoes 400g', 50, 100);
-INSERT INTO ingredient VALUES (5, 'Beef Stock (goo pot)', 35, 15);
-INSERT INTO ingredient VALUES (6, 'Jalapenos 100g', 65, 16);
-INSERT INTO ingredient VALUES (7, 'Kidney Beans 400g', 50, 280);
-INSERT INTO ingredient VALUES (8, 'Carrots x2', 10, 20);
-INSERT INTO ingredient VALUES (9, 'Parmesan 100g', 135, 400);
-INSERT INTO ingredient VALUES (10, 'Red Wine (glass)', 100, 120);
-INSERT INTO ingredient VALUES (11, 'Spaghetti 500g', 100, 1750);
+INSERT INTO ingredient VALUES (1, 'Minced Beef 500g', 400);
+INSERT INTO ingredient VALUES (2, 'Onion', 20);
+INSERT INTO ingredient VALUES (3, 'Garlic', 30);
+INSERT INTO ingredient VALUES (4, 'Chopped Tomatoes 400g', 50);
+INSERT INTO ingredient VALUES (5, 'Beef Stock (goo pot)', 35);
+INSERT INTO ingredient VALUES (6, 'Jalapenos 100g', 65);
+INSERT INTO ingredient VALUES (7, 'Kidney Beans 400g', 50);
+INSERT INTO ingredient VALUES (8, 'Carrots x2', 10);
+INSERT INTO ingredient VALUES (9, 'Parmesan 100g', 135);
+INSERT INTO ingredient VALUES (10, 'Red Wine (glass)', 100);
+INSERT INTO ingredient VALUES (11, 'Spaghetti 500g', 100);
+INSERT INTO ingredient VALUES (12, 'Broccoli', 40);
+INSERT INTO ingredient VALUES (13, 'Chestnut Mushrooms 250g', 100);
+INSERT INTO ingredient VALUES (14, 'Double Cream 300ml', 90);
+INSERT INTO ingredient VALUES (15, 'White Wine (glass)', 100);
+INSERT INTO ingredient VALUES (16, 'Pasta 500g', 100);
 
 INSERT INTO meal_ingredient_bridge VALUES (1, 1);
 INSERT INTO meal_ingredient_bridge VALUES (1, 2);
@@ -57,5 +62,10 @@ INSERT INTO meal_ingredient_bridge VALUES (2, 8);
 INSERT INTO meal_ingredient_bridge VALUES (2, 9);
 INSERT INTO meal_ingredient_bridge VALUES (2, 10);
 INSERT INTO meal_ingredient_bridge VALUES (2, 11);
-
+INSERT INTO meal_ingredient_bridge VALUES (3, 3);
+INSERT INTO meal_ingredient_bridge VALUES (3, 12);
+INSERT INTO meal_ingredient_bridge VALUES (3, 13);
+INSERT INTO meal_ingredient_bridge VALUES (3, 14);
+INSERT INTO meal_ingredient_bridge VALUES (3, 15);
+INSERT INTO meal_ingredient_bridge VALUES (3, 16);
 COMMIT;
